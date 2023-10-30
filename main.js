9adff1e9b7dc3c1bd6a52b2e937d097cb7a3f888
emran <emranbspt@gmail.com>
2023-10-30 15:38:25 +0600

    trying vue

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const app = Vue.createApp({
     data:function(){
         return{
             say: 'Trying something...',
+            p: `trying vue js, its quite good, lets see how
+            far i can get with this
+            `
         }
     }
 })
