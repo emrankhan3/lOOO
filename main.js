cfcedd897e29931f14505576484ba40a3db9d295
emran <emranbspt@gmail.com>
2023-10-30 15:53:14 +0600

    add style bla bla

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             say: 'Trying something...',
             p: `trying vue js, its quite good, lets see how
             far i can get with this
-            `
+            `,
+            dd: 'bg-green-100',
+            loc:`assets/ch.jpg`
         }
     }
 })
